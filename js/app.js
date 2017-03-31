77478b409e8ebaa95d0760d57b588d46178ab6e2
HKeating <horacekeating@hotmail.co.uk>
2017-03-31 16:28:00 +0100

    began pseudocoding for flappy sausage and experimenting with animating divs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('Hi there');
+
+
+$(() =>{
+
+  const $board = $('.gameBoard');
+  const $sausage = $(document.createElement('div'));
+  $($sausage).addClass('sausage');
+  $($board).append($sausage);
+
+
+  // setInterval(function() {
+  //   $($sausage).style.top = '-1px';
+  //     // If the top position is greater than 100px, set it to 100px
+  //   if (parseInt($($sausage).style.top) < 400) { $($sausage).style.top = '100px'; }
+  // }, 200);
+
+
+
+
+
+
+});
