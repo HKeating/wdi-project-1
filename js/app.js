9b3bc61fb7f9b12578fd57f65a37b5112176b99c
HKeating <horacekeating@hotmail.co.uk>
2017-03-31 17:14:56 +0100

    set up basic animation of object falling

--- js/app.js
+++ js/app.js
@@ -5,15 +5,19 @@ $(() =>{
 
   const $board = $('.gameBoard');
   const $sausage = $(document.createElement('div'));
+
   $($sausage).addClass('sausage');
   $($board).append($sausage);
 
 
-  // setInterval(function() {
-  //   $($sausage).style.top = '-1px';
-  //     // If the top position is greater than 100px, set it to 100px
-  //   if (parseInt($($sausage).style.top) < 400) { $($sausage).style.top = '100px'; }
-  // }, 200);
+  console.log($sausage.offset());
+  // let $sausageTop = $sausage.position().top;
+
+
+  $sausage.animate({ top: '535px' }, 1000);
+
+
+
 
 
 
