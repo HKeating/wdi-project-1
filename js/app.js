f7f535eff5bf6fb9d2ad0956f4b257da1cec3d09
HKeating <horacekeating@hotmail.co.uk>
2017-04-03 12:30:20 +0100

    basic refactoring to neaten up code before adding obstacles

--- js/app.js
+++ js/app.js
@@ -7,38 +7,45 @@ $(() =>{
   const $sausage = $(document.createElement('div'));
   const $start = $('#start');
   const $obstacle = $(document.createElement('div'));
-
+  
   $($sausage).addClass('sausage');
   $($sausage).text('Sausage');
   $($sausage).css('left', ($($board).css('left')+10));
-  $($sausage).css('top', (490));
+  $($sausage).css('top', 490);
   $($board).append($sausage);
   $($obstacle).addClass('obstacle');
   $($board).append($obstacle);
 
   // console.log($($board).css('height'));
-  // $($start).on('click', function() {
-  $(document).on('keydown', function() {
+  $($start).on('click', function() {
+    $(document).keydown(jump);
+  });
+
+  function jump() {
     $sausage.clearQueue();
     $sausage.stop();
     $sausage.animate({ top: '-=80px'}, 200);
     atTop();
     atBottom();
-  });
-  // });
+  }
 
 
-  const atBottom = function() {
+
+  function atBottom() {
     if ($sausage.position().top < 490) {
       $sausage.animate({ top: '490px' }, 1000);
     }
-  };
+  }
 
-  const atTop = function() {
+  function atTop() {
     if( $sausage.position().top < 70) {
       $sausage.clearQueue();
       $sausage.stop();
     }
-  };
+  }
+
+  // function createObstacle() {
+  //
+  // }
 
 });
