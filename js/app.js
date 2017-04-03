3f8a8ee22d27d25aebe823ae741e3d82a6564312
HKeating <horacekeating@hotmail.co.uk>
2017-04-03 12:19:56 +0100

    found problem causing obstacle to break code. commented out start button for ease of development

--- js/app.js
+++ js/app.js
@@ -6,26 +6,26 @@ $(() =>{
   const $board = $('.gameBoard');
   const $sausage = $(document.createElement('div'));
   const $start = $('#start');
-  // const $obstacle = $(document.createELement('div'));
+  const $obstacle = $(document.createElement('div'));
 
   $($sausage).addClass('sausage');
   $($sausage).text('Sausage');
   $($sausage).css('left', ($($board).css('left')+10));
   $($sausage).css('top', (490));
-  // $($obstacle).addClass('obstacle');
   $($board).append($sausage);
-  // $($board).append($obstacle);
+  $($obstacle).addClass('obstacle');
+  $($board).append($obstacle);
 
   // console.log($($board).css('height'));
-  $($start).on('click', function() {
-    $(document).on('keydown', function() {
-      $sausage.clearQueue();
-      $sausage.stop();
-      $sausage.animate({ top: '-=80px'}, 200);
-      atTop();
-      atBottom();
-    });
+  // $($start).on('click', function() {
+  $(document).on('keydown', function() {
+    $sausage.clearQueue();
+    $sausage.stop();
+    $sausage.animate({ top: '-=80px'}, 200);
+    atTop();
+    atBottom();
   });
+  // });
 
 
   const atBottom = function() {
